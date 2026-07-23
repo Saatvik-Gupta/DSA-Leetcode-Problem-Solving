08e49be6a871899c382369e34840b3c13c5c5409
SaatvikTechno <shobhita65403@gmail.com>
2026-07-23 19:52:07 +0530

    Time: 441 ms (78.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT today.id
+FROM Weather AS today
+JOIN Weather AS yesterday
+ON yesterday.recordDate = DATE_SUB(today.recordDate, INTERVAL 1 DAY)
+WHERE today.temperature > yesterday.temperature;
