12044e264deaca11b67a4291225e62d256321a98
SaatvikTechno <shobhita65403@gmail.com>
2026-07-26 09:07:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (10.23%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -2,7 +2,28 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
 
-       return n;
+      // return n; 
+
+       int sumodd=0;
+        int sumeven=0;
+
+        for(int i=1; i<=n; i++){
+            sumodd+=(2*i)-1;
+
+            sumeven+= 2*i;
+        }
+
+         while(sumodd>0 && sumeven>0){
+            if(sumodd>sumeven){
+                sumodd=sumodd%sumeven;
+            }
+            else{
+                sumeven=sumeven%sumodd;
+            }
+        }
+        if(sumodd==0) return sumeven;
+        return sumodd;
+        
     }
 
 };
