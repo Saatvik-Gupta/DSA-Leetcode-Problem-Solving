7d688409522f62ba27a2468c5c9b8b4dc11a7ac1
SaatvikTechno <shobhita65403@gmail.com>
2026-07-27 07:45:03 +0530

    Time: 3 ms (39.15%) | Memory: 8.2 MB (62.71%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+
+     bool isvalid(vector<string>& board,int n,int row,int col){
+        // Checking horizontal
+        for(int j=0; j<n;j++){
+            if(board[row][j]=='Q') return false;
+        }
+
+        // checking vertical
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q') return false;
+        }
+
+        // left diagaonal
+        for(int i=row,j=col; i>=0 && j>=0; i--,j--){
+            if(board[i][j]=='Q') return false;
+        }
+
+        // Right diagaonal
+        for(int i=row,j=col; i>=0 && j<n; i--,j++){
+            if(board[i][j]=='Q') return false;
+        }
+
+        return true;
+    }
+
+    void nQueens(vector<string>& board,int n,int row,int &count){
+        if(row==n){ // base case
+            count++;
+            return;
+        }
+
+        for(int j=0; j<n; j++){
+            if(isvalid(board,n,row,j)){
+                board[row][j]='Q';
+                nQueens(board,n,row+1,count);
+                board[row][j]='.'; // backtrack
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        
+        vector<string>board(n,string(n,'.'));
+        vector<vector<string>> ans;
+        int count=0;
+        nQueens(board,n,0,count);
+
+        return count;
+
+        
+        
+    }
+};
