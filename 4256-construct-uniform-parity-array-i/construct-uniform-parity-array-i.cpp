7bf1dcd99317da027613f80e558c11412c6920bd
SaatvikTechno <shobhita65403@gmail.com>
2026-09-02 08:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        
+        bool hasOdd = false;
+        bool hasEven = false;
+
+        for (int x : nums1) {
+            if (x % 2 == 0)
+                hasEven = true;
+            else
+                hasOdd = true;
+        }
+
+        // If all numbers already have the same parity
+        if (!hasOdd || !hasEven)
+            return true;
+
+        // If both odd and even exist,
+        // subtract an odd number from every even number.
+        return true;
+    }
+};
